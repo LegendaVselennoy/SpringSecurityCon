b08c8a27d63ad4d9fc2291699448b550586602ae
LegendaVselennoy <103209589+LegendaVselennoy@users.noreply.github.com>
2024-08-31 21:20:38 +0300

    sec server

---
+++ securityserver/src/test/java/org/example/securityserver/SecurityserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
