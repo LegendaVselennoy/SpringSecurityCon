0b4301fc3bb51e6a1870c0186b3990829c3aa5b5
LegendaVselennoy <103209589+LegendaVselennoy@users.noreply.github.com>
2024-09-03 15:01:36 +0300

    Config Server

--- spring-micro-demo/eureka-client/src/main/java/org/example/controller/TestController.java
+++ spring-micro-demo/eureka-client/src/main/java/org/example/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String port;
+
     @GetMapping()
     public String test() {
-        return "test";
+        return port;
     }
 }
