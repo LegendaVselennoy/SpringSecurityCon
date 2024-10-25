e219aaf153a76180ab066f680028119723036de9
LegendaVselennoy <103209589+LegendaVselennoy@users.noreply.github.com>
2024-10-25 17:48:00 +0300

    Use Grandle

---
+++ catalog-service/src/main/java/com/bookshop/configuration/PolarProperties.java
@@ -0,0 +1,12 @@
+package com.bookshop.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "polar")
+@Getter @Setter
+public class PolarProperties {
+    private String greeting;
+}
---
+++ catalog-service/src/main/java/com/bookshop/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.bookshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+//    private final PolarProperties polarProperties;
+
+//    @GetMapping("/")
+//    public String getGreeting() {
+//        return polarProperties.getGreeting();
+//    }
+}
